618c454deda7d66dcd0027f35719c305cd13853e
unknown <sebastiengithub@gmail.com>
2025-10-01 07:07:50 +0200

    Mise en place d'un snackBar pour la création d'un jeu vidéo

--- projects/gameotheque/src/app/features/games/components/create-video-game/create-video-game.component.ts
+++ projects/gameotheque/src/app/features/games/components/create-video-game/create-video-game.component.ts
@@ -1,25 +1,42 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormVideoGameComponent } from '../form-video-game/form-video-game.component';
 import { VideoGame } from '../../../models/video-game';
 import { SaveOneVideoGameService } from '../../services/save-one-video-game.service';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {
+  MatSnackBar,
+  MatSnackBarAction,
+  MatSnackBarActions,
+  MatSnackBarLabel,
+  MatSnackBarRef,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-create-video-game',
   standalone: true,
-  imports: [FormVideoGameComponent],
+  imports: [FormVideoGameComponent, MatProgressSpinnerModule],
   templateUrl: './create-video-game.component.html',
   styleUrl: './create-video-game.component.css'
 })
 export class CreateVideoGameComponent {
   private readonly service = inject(SaveOneVideoGameService)
+  private readonly snackbar = inject(MatSnackBar)
+  isSaving = signal(false)
+
+   openSnack(message: string) {
+    this.snackbar.open(message, '', { duration: 1000 })
+    }
 
   saveOne(item: VideoGame): void {
+    this.isSaving.set(true)
     this.service.save(item).subscribe({
       next: savedVideoGame => {
-        console.info('Sauvegarde Réussie !')
+        this.openSnack('Sauvegarde réussi')
+        this.isSaving.set(false)
       },
       error: err => {
-        console.info('Oops prob lors de la sauvegarde')
+        this.openSnack('Oops prob lors de la sauvegarde')
+        this.isSaving.set(false)
       }
     })
   }
